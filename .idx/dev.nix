7c0a04f1c241650f779cb340d67d601f84d54288
victorygameth <victorygameth@gmail.com>
2026-03-22 14:53:45 +0000

    Initial commit

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,50 +1,54 @@
 # To learn more about how to use Nix to configure your environment
-# see: https://developers.google.com/idx/guides/customize-idx-env
+# see: https://firebase.google.com/docs/studio/customize-workspace
 { pkgs, ... }: {
   # Which nixpkgs channel to use.
-  channel = "stable-25.05";
+  channel = "stable-24.05"; # or "unstable"
+
   # Use https://search.nixos.org/packages to find packages
-  packages = [ pkgs.nodejs_20 pkgs.nodejs_20 pkgs.jdk21_headless pkgs.gradle ];
+  packages = [
+    # pkgs.go
+    # pkgs.python311
+    # pkgs.python311Packages.pip
+    # pkgs.nodejs_20
+    # pkgs.nodePackages.nodemon
+  ];
+
   # Sets environment variables in the workspace
-  env = { EXPO_USE_FAST_RESOLVER = 1; };
+  env = {};
   idx = {
     # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
     extensions = [
-      "msjsdiag.vscode-react-native"
-      "fwcd.kotlin"
+      # "vscodevim.vim"
     ];
-    workspace = {
-      # Runs when a workspace is first created with this `dev.nix` file
-      onCreate = {
-        install-and-prebuild = ''
-          npm ci --prefer-offline --no-audit --no-progress --timing && npm i @expo/ngrok@^4.1.0 react@latest react-dom@latest react-native@latest && npm i -D @types/react@latest && npx -y expo install expo-dev-client && npx -y expo prebuild --platform android
-          # Add more memory to the JVM
-          sed -i 's/org.gradle.jvmargs=-Xmx2048m -XX:MaxMetaspaceSize=512m/org.gradle.jvmargs=-Xmx4g -XX:MaxMetaspaceSize=512m/' "android/gradle.properties"
-        '';
-      };
-      # Runs when a workspace restarted
-      onStart = {
-        android = ''
-          echo -e "\033[1;33mWaiting for Android emulator to be ready...\033[0m"
-          # Wait for the device connection command to finish
-          adb -s emulator-5554 wait-for-device && \
-          npm run android
-        '';
-      };
-    };
-    # Enable previews and customize configuration
+
+    # Enable previews
     previews = {
       enable = true;
       previews = {
-        web = {
-          command = [ "npm" "run" "web" "--" "--port" "$PORT" ];
-          manager = "web";
-        };
-        android = {
-          # noop
-          command = [ "tail" "-f" "/dev/null" ];
-          manager = "web";
-        };
+        # web = {
+        #   # Example: run "npm run dev" with PORT set to IDX's defined port for previews,
+        #   # and show it in IDX's web preview panel
+        #   command = ["npm" "run" "dev"];
+        #   manager = "web";
+        #   env = {
+        #     # Environment variables to set for your server
+        #     PORT = "$PORT";
+        #   };
+        # };
+      };
+    };
+
+    # Workspace lifecycle hooks
+    workspace = {
+      # Runs when a workspace is first created
+      onCreate = {
+        # Example: install JS dependencies from NPM
+        # npm-install = "npm install";
+      };
+      # Runs when the workspace is (re)started
+      onStart = {
+        # Example: start a background task to watch and re-build backend code
+        # watch-backend = "npm run watch-backend";
       };
     };
   };
---
+++ server-api/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 3005;
+
+app.get('/', (req, res) => {
+  res.send('Server is running!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
